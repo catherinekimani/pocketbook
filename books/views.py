b6c10a88786a1763194ec4e578646e795a5b0679
catherinekimani <catherinekimani882@gmail.com>
2022-12-11 21:00:00 +0100

    Add a register function to the views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .forms import NewUserForm #newuserform class
+from django.contrib.auth import login
+from django.contrib import messages #import messages
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def signup(request):
+    form = NewUserForm()
+    
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'accounts/register.html',{'form':form})
+    # 23979841
+
