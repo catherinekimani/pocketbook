23dd89e9eaed3a2ae166f57badceafc12bdf897c
catherinekimani <catherinekimani882@gmail.com>
2022-12-12 19:00:00 +0100

    remove firstname & lastname fiels

--- books/forms.py
+++ books/forms.py
@@ -4,12 +4,10 @@ from django.contrib.auth.models import User
 
 # forms
 class NewUserForm(UserCreationForm):
-    firstname = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
-    lastname = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
-    email  =    forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control'}))
+    email  =   forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control'}))
     class Meta:
         model = User
-        fields = ['username','firstname','lastname','email','password1','password2']
+        fields = ['username','email','password1','password2']
         
     def __init__(self,*args, **kwargs):
         super(NewUserForm, self).__init__(*args,**kwargs)
