9abb577e10f715462bdcd01b6caba13e298da2f5
catherinekimani <catherinekimani882@gmail.com>
2022-12-14 11:00:00 +0100

    Update forms.py file

--- books/forms.py
+++ books/forms.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 
 # forms
 class NewUserForm(UserCreationForm):
-    email  =   forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control'}))
+    email=forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control'}))
+    
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
         
     def __init__(self,*args, **kwargs):
         super(NewUserForm, self).__init__(*args,**kwargs)
-        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['class'] = 'form-control' 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
